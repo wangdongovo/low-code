50b7c78dd6726f7df34587c7e5b5fc4907b1c818
Martina Emerson <wangdongovo@gmail.com>
2024-06-25 10:40:57 +0800

    fix: /

--- src/pages/editor/components/left-panel/index.tsx
+++ src/pages/editor/components/left-panel/index.tsx
@@ -1,11 +1,42 @@
+import { useState } from 'react'
 import './index.less'
 
-
-
 const LeftPanel = () => {
-    return (
-        <div className="left-panel">left panel</div>
+  const [tabBar, setTabBar] = useState([
+    {
+      id: 1,
+      name: '组件',
+      active: true
+    },
+    {
+      id: 2,
+      name: '资源',
+      active: false
+    }
+  ])
+
+  const handleTab = (item: any) => {
+    const updatedTabBar = tabBar.map((tab: any) =>
+      tab.id === item.id ? { ...tab, active: true } : { ...tab, active: false }
     )
+    setTabBar(updatedTabBar)
+  }
+
+  return (
+    <div className="left-panel">
+      <div className="tab-bar">
+        {tabBar.map((item: any) => (
+          <div
+            className={item.active ? 'tab-item active' : 'tab-item'}
+            key={item.id}
+            onClick={() => handleTab(item)}
+          >
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default LeftPanel
